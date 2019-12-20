f1d1d340dae81f05622d0432200098367425da85
Luis Carlos Hernández Peña <lucashernandez067@gmail.com>
2019-12-20 00:07:55 -0500

    Update welcome.blade.php

--- src/CocinaRiquisimo/resources/views/welcome.blade.php
+++ src/CocinaRiquisimo/resources/views/welcome.blade.php
@@ -2,15 +2,26 @@
 @section('title', 'Bienvenido')
 @section('content')
     <div class="row">
-        <div class="col">
+        <center>
+        <div class="col-4 mb-3">
             <img src="img/icons/main.png" alt="" width="300" height="auto">
         </div>
-        <div class="col">
+        <div class="col-4">
             Este proyecto busca que las personas puedan encontrar recetas fácilmente
             ya que con esta página web podrían buscar sus recetas preferidas de la mano
             de su comunidad generando un entonrno social y de sana convivencia entre los 
             usuarios que así lo deseen.
+
+            También permite que sus usuarios agreguen sus recetas propias para que otros 
+            puedan conocerlas.
+
+            Muchas veces nos encontramos en la problematica de no saber cómo prepararnos 
+            nuestras comidas preferidas, cuando estás sólo y buscas por internet, te puedes
+            sentir algo perdido, pero esto es cosa del pasado, con nuestra app web sé capaz de
+            cocinar tus platos preferidos con la asistencia de la comunidad y el servicio que 
+            estás necesitando.
         </div>
+        </center>
     </div>
     
 @endsection
