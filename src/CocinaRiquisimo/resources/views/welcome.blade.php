4995b630ce019971f7232b8a388a45d8a0ebb799
Luis Carlos Hernández Peña <lucashernandez067@gmail.com>
2019-12-19 23:59:34 -0500

    un pequeño guiño

--- src/CocinaRiquisimo/resources/views/welcome.blade.php
+++ src/CocinaRiquisimo/resources/views/welcome.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.main')
 @section('title', 'Bienvenido')
 @section('content')
+    <div class="row">
+        <div class="col">
+            <img src="img/icons/main.png" alt="" width="300" height="auto">
+        </div>
+        <div class="col">
+            Este proyecto busca que las personas puedan encontrar recetas fácilmente
+            ya que con esta página web podrían buscar sus recetas preferidas de la mano
+            de su comunidad generando un entonrno social y de sana convivencia entre los 
+            usuarios que así lo deseen.
+        </div>
+    </div>
     
 @endsection
